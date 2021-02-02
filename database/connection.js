6c3edc439acab3895efa869fe40b4f945442797b
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-02-02 20:39:46 -0300

    test

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 
-const user =  secrets.USER;
-const database =  secrets.DATABASE;
-const password =  secrets.PASSWORD;
-const host =  secrets.HOST;
-const database_type =  secrets.DATABASE_TYPE;
+const user =  process.env.USER;
+const database =  process.env.DATABASE;
+const password =  process.env.PASSWORD;
+const host =  process.env.HOST;
+const database_type =  process.env.DATABASE_TYPE;
 
 const connection = new Sequelize(user, database, password, {
     host: host,
