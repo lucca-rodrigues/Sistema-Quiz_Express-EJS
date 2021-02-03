d4a1c790e2a829dc9562a3ae7c10d2db5dc51f24
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-02-03 10:22:39 -0300

    test

--- database/connection.js
+++ database/connection.js
@@ -3,11 +3,12 @@ const Sequelize = require('sequelize');
 const user =  process.env.USER;
 const database =  process.env.DATABASE;
 const password =  process.env.PASSWORD;
+const database_type =  process.env.DATABASE_TYPE;
 // const host =  process.env.HOST;
 
 const connection = new Sequelize(user, database, password, {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: database_type
 });
 
 module.exports = connection;
