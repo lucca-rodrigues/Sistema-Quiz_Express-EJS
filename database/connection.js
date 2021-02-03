737c309fc7f9ec2a364b577263f2fed01a5ab950
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-02-03 13:11:17 -0300

    test

--- database/connection.js
+++ database/connection.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
 
-const user =  process.env.USER;
-const database =  process.env.DATABASE;
-const password =  process.env.PASSWORD;
+const user =  process.env.APP_USER;
+const database =  process.env.APP_DATABASE;
+const password =  process.env.APP_PASSWORD;
 // const database_type =  process.env.DATABASE_TYPE;
-const host =  process.env.HOST;
-const port =  process.env.PORT;
+const host =  process.env.APP_HOST;
+const port =  process.env.APP_PORT;
 
 const connection = new Sequelize(user, database, password, {
     host: host,
     port: port,
-    dialect: 'pg'
+    dialect: 'postgresql'
 });
 
 module.exports = connection;
